087c2a204aa4b0620d5523d9d84f323af2583e88
alinebeda <9097@holbertonstudents.com>
2024-02-20 13:31:12 +0000

    de

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-printf("Size of int: %zu bytes\n", sizeof(int));
-printf("Size of float: %zu bytes\n", sizeof(float));
-printf("Size of double: %zu bytes\n", sizeof(double));
-printf("Size of char: %zu bytes\n", sizeof(char));
-printf("Size of 167: %zu bytes\n", sizeof(167));
-printf("Size of 3.1415926: %zu bytes\n", sizeof(3.1415926));
-printf("Size of '$': %zu bytes\n", sizeof('$'));
+	printf("Size of int: %zu bytes\n", sizeof(int));
+	printf("Size of float: %zu bytes\n", sizeof(float));
+	printf("Size of double: %zu bytes\n", sizeof(double));
+	printf("Size of char: %zu bytes\n", sizeof(char));
+	printf("Size of 167: %zu bytes\n", sizeof(167));
+	printf("Size of 3.1415926: %zu bytes\n", sizeof(3.1415926));
+	printf("Size of '$': %zu bytes\n", sizeof('$'));
 
-return (0);
+	return (0);
 }
 
