065c347d7a56e97753e2fca3b7a39b9cee281cf4
alinebeda <9097@holbertonstudents.com>
2024-02-20 11:43:48 +0000

    mygod

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-    // Print the desired text using puts function
     puts("Programming is like building a multilingual puzzle");
-
-    // Return 0 to indicate successful completion
     return 0;
 }
+
