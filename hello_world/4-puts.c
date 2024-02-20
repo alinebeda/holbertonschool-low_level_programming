d1fbe0951c846a881b187f62084497887b51529a
alinebeda <9097@holbertonstudents.com>
2024-02-20 11:26:20 +0000

    f

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#incluse <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    /* Use puts to print a string followed by a new line */
+    puts("Programming is like building a multilingual puzzle");
+
+    /* Return 0 to indicate success */
+    return (0);
+}
