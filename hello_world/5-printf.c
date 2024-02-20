6f0ee111e133d3a5a48d6d255030d5fbca02fe7a
alinebeda <9097@holbertonstudents.com>
2024-02-20 12:54:21 +0000

    sq

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("You are not allowed to use the function puts,\n");
+printf("but the outcome is a piece of art,\n");
+printf("followed by a new line.\n");
+return (0);
+}
