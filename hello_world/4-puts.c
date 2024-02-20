d7f5d7537184dc21742cab3e037d10a81703edd2
alinebeda <9097@holbertonstudents.com>
2024-02-20 11:40:23 +0000

    g

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,9 @@
-#incluse <stdio.h>
-/**
- * main - A program that prints a line with puts function
- * Return: 0 (Success)
- */
-int main(void)
-{
-puts("Programming is like building a multilingual puzzle");
-return (0);
+#include <stdio.h>
+
+int main(void) {
+    // Print the desired text using puts function
+    puts("Programming is like building a multilingual puzzle");
+
+    // Return 0 to indicate successful completion
+    return 0;
 }
