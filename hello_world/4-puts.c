79b272736eddcb38fb99749819773bcb1d572b9f
alinebeda <9097@holbertonstudents.com>
2024-02-20 11:34:48 +0000

    v

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #incluse <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
  */
 int main(void)
 {
-    /* Use puts to print a string followed by a new line */
-    puts("Programming is like building a multilingual puzzle");
-
-    /* Return 0 to indicate success */
-    return (0);
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }
