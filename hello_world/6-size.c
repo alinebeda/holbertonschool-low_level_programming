99f301c4d2196747006b91953bdb56f707f2c9ba
alinebeda <9097@holbertonstudents.com>
2024-02-20 13:28:08 +0000

    nigth

---
+++ hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of int: %zu bytes\n", sizeof(int));
+printf("Size of float: %zu bytes\n", sizeof(float));
+printf("Size of double: %zu bytes\n", sizeof(double));
+printf("Size of char: %zu bytes\n", sizeof(char));
+printf("Size of 167: %zu bytes\n", sizeof(167));
+printf("Size of 3.1415926: %zu bytes\n", sizeof(3.1415926));
+printf("Size of '$': %zu bytes\n", sizeof('$'));
+
+return (0);
+}
+
