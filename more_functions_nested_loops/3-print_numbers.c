f1411d69f3fe4fb4393b47f012ccf1880d8cab12
alinebeda <9097@holbertonstudents.com>
2024-02-29 19:34:34 +0000

    up

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_numbers - imprime les chiffres de 0 a 9
+ *
+ */
+void print_numbers(void)
+{
+	int i;
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
