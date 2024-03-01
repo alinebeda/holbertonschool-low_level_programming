b60ad7c7da90cab65211604ac20c6fa7ceaf5469
alinebeda <9097@holbertonstudent.com>
2024-03-01 23:57:34 +0000

    yes

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -7,19 +7,26 @@
  */
 
 void print_diagonal(int n)
-
 {
-	int i, j;
-
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
+		_putchar('\n');
+		return;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
 		_putchar('\\');
 		_putchar('\n');
 	}
-	for (j = 0; j < i; j++)
-	{
-		_putchar(' ');
-	}
+}
+int main(void)
+{
 	print_diagonal(5);
 	return (0);
 }
+
