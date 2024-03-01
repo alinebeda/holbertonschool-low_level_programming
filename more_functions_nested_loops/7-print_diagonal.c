f0a611a9e05daa02a12b8188272e237469d6caa1
alinebeda <9097@holbertonstudent.com>
2024-03-01 12:50:13 +0000

    de

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ *print_diagonal - imprime une ligne diagonale.
+ *@n: la longueur de la ligne
+ */
+
+void print_diagonal(int n)
+
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('\\');
+		_putchar('\n');
+	}
+	for (j = 0; j < i; j++)
+	{
+		_putchar(' ');
+	}
+}
