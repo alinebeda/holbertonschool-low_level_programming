e5af2374fc77a42027408962fadda7d4fcdad310
alinebeda <9097@holbertonstudent.com>
2024-03-07 19:56:14 +0000

    hope

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+
+void _puts_recursion(char *s)
+
+{
+	if (*s == '\0')
+	{
+		printf("\n");
+		return;
+	}
+	printf("%c", *s);
+	_puts_recursion(s + 1);
+}
