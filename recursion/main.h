0c5f506b8db501d5aa6a2126225a70fcd4ba322a
alinebeda <9097@holbertonstudent.com>
2024-03-07 20:13:17 +0000

    hopely

---
+++ recursion/main.h
@@ -0,0 +1,18 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0  (Success)
+ */
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+#endif /*Main_H*/
