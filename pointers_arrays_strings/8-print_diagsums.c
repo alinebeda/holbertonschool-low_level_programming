f1f25155c932764a248b7d83a2ead3f509e9e040
alinebeda <9097@holbertonstudent.com>
2024-03-07 12:35:53 +0000

    need

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -9,15 +9,16 @@
  */
 
 void print_diagsums(int *a, int size)
-{
-	int sum1 = 0;
-	int sum2 = 0;
 
-	for (int i = 0; i < size; i++)
+{
+
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
 	{
 		sum1 += *(a + i * size + i);
 	}
-	for (int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		sum2 += *(a + i * size + (size - 1 - i));
 	}
