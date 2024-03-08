43e1eaeeb9afa9ecb68723d65210398302d0b26c
alinebeda <9097@holbertonstudent.com>
2024-03-08 11:19:45 +0000

    task1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _print_rev_recursion - Prints a string in reverse
@@ -12,7 +11,7 @@ void _print_rev_recursion(char *s)
 	if (*s)
 	{
 		_print_rev_recursion(s + 1);
-		putchar(*s);
+		_putchar(*s);
 	}
 }
 
