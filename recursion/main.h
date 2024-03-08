0966844b4bb98a0a027a0b60749f5e0dd5f918b1
alinebeda <9097@holbertonstudent.com>
2024-03-08 11:14:46 +0000

    add main

--- recursion/main.h
+++ recursion/main.h
@@ -1,13 +1,4 @@
-#ifndef __MAIN_H__
-#define __MAIN_H__
-#include <unistd.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0  (Success)
- */
-
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -15,4 +6,3 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-#endif /*Main_H*/
