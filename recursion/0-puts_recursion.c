e9f3a03f7da895129d30e455c9de4c869efdb964
alinebeda <9097@holbertonstudent.com>
2024-03-08 11:13:47 +0000

    hgfejhfv

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -8,12 +8,12 @@
 void _puts_recursion(char *s)
 
 {
-	if (s[0] != '\0')
+	if (*s == '\0')
+		_putchar('\n');
+
+	else if (s[0] != '\0')
 	{
 		_putchar(s[0]);
 		_puts_recursion(s + 1);
 	}
-	else
-		_putchar('\n');
-
 }
