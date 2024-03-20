f9a4deb0113bd431895cbce0bea329152f9271b7
alinebeda <9097@holbertonstudent.com>
2024-03-20 21:13:12 +0000

    hope

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print_name - print name using printer to function
+ * @name: String to add
+ * @f: Pointer to function
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
