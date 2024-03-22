28ffe0cd22920a918cb4ad5347dd064472a2d56d
alinebeda <9097@holbertonstudent.com>
2024-03-22 22:00:10 +0000

    yes

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (str == NULL)
 			printf("(nil)");
 		else
-			printf("%d", str);
+			printf("%s", str);
 		if (index != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
